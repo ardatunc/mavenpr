1b1ab5a93d859371c4309f6651b01879c20ee493
Administrator <Administrator@192.168.1.25>
2022-02-13 14:13:39 +0300

    For test,Student Repository in memory implementation added.

---
+++ studentmanagement/src/main/java/com/mavenschool/studentmanagement/dao/mem/StudentRepositoryInMemoryImpl.java
@@ -0,0 +1,73 @@
+package com.mavenschool.studentmanagement.dao.mem;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Repository;
+
+import com.mavenschool.studentmanagement.dao.StudentRepository;
+import com.mavenschool.studentmanagement.model.Student;
+
+
+@Repository // Test İmplementasyonu
+public class StudentRepositoryInMemoryImpl implements StudentRepository {
+	
+	private Map<Integer,Student> studentsMap = new HashMap<>();
+	
+
+	public StudentRepositoryInMemoryImpl() {
+		Student student1 = new Student();
+		student1.setId(1703);
+		student1.setFirstName("Arda");
+		student1.setLastName("Tunc");
+		
+		Student student2 = new Student();
+		student2.setId(1234);
+		student1.setFirstName("Sait");
+		student1.setLastName("Yıkılmaz");
+	
+		studentsMap.put(student1.getId(), student1);
+		studentsMap.put(student2.getId(), student2);
+	}
+	
+	@Override
+	public List<Student> findAll() {
+		return new ArrayList<>(studentsMap.values());
+	}
+
+	@Override
+	public Student findById(Integer id) {
+		return studentsMap.get(id);
+		
+	}
+
+	@Override
+	public List<Student> findByLastName(String lastName) {
+		return studentsMap.values().stream().filter(o->o.getLastName().equals(lastName)).collect(Collectors.toList());
+	}
+
+	@Override
+	public void create(Student student) {
+		student.setId(123455);
+		studentsMap.put(student.getId(),student);
+		
+
+	}
+
+	@Override
+	public Student update(Student student) {
+		studentsMap.replace(student.getId(), student);
+		return student;
+	}
+
+	@Override
+	public void delete(Integer id) {
+		studentsMap.remove(id);
+
+	}
+
+}
